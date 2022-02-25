8a3d164890bd936a9d07b59fd3a21b41ba9b5ddb
Abby Henes <abbyhenes@gmail.com>
2022-02-25 11:13:32 -0600

    started controller

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -1,4 +1,15 @@
 package com.example.menu.item;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/menu/items")
 public class ItemController {
+
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        this.service = service;
+    }
 }
