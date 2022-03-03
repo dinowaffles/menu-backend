5f618e2eb4d174fe9a32ae7f0edece826e5acd30
Abby Henes <abbyhenes@gmail.com>
2022-03-03 15:58:19 -0600

    added exception and item annotations

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -1,7 +1,19 @@
 package com.example.menu.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/menu/items")
@@ -11,5 +23,60 @@ public class ItemController {
 
     public ItemController(ItemService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> findAll() {
+        List<Item> items = service.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> find(@PathVariable("id") Long id) {
+        Optional<Item> item = service.find(id);
+        return ResponseEntity.of(item);
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> create(@Valid @RequestBody Item item) {
+        Item created = service.create(item);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> update(
+            @PathVariable("id") Long id, @Valid @RequestBody Item updatedItem) {
+        Optional<Item> updated = service.update(id, updatedItem);
+        return updated.map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    Item created = service.create(updatedItem);
+                    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(created.getId())
+                            .toUri();
+                    return ResponseEntity.created(location).body(created);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Item> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return ResponseEntity.badRequest().body(map);
     }
 }
